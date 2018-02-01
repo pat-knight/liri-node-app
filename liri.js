73f7b6a8b8c48dd3ca00ebbbc792157ea57de808
Patrick Knight <ptknight00@gmail.com>
2018-01-31 19:45:23 -0600

    env

--- liri.js
+++ liri.js
@@ -1,28 +1,59 @@
 // Node module imports needed to run the functions
 require("dotenv").config();
-var fs = require("fs"); //reads and writes files
-var request = require("request");
-var keys = require("./keys.js");
-var twitter = require("twitter");
-var Spotify = require('node-spotify-api');
-var userCommand = process.argv[2];
+const fs = require("fs"); //reads and writes files
+const request = require("request");
+const keys = require("./keys.js");
+const twitter = require("twitter");
+const Spotify = require('node-spotify-api');
+const userCommand = process.argv[2];
 
 switch(userCommand){
-    //use node liri.js my-tweets to:
-    //show last 20 tweets with timestamp in bash window
     case 'my-tweets':
+        tweets();
     break;
-    //use node liri.js my-tweets to:
-    //show last 20 tweets with timestamp in bash window
+    
     case 'spotify-this-song':
+        spotify();
     break;
-    //use node liri.js my-tweets to:
-    //show last 20 tweets with timestamp in bash window
+    
     case 'movie-this':
+        movie();
     break;
-    //use node liri.js my-tweets to:
-    //show last 20 tweets with timestamp in bash window
+    
     case 'do-what-it-says':
+        doWhat();
     break;
 }
 
+//use node liri.js my-tweets to:
+//show last 20 tweets with timestamp in bash window
+tweets =>{
+    var client = new twitter(twitter.keys);
+    var tweets = {
+        user_id: webdevpat,
+        count: 20
+    };
+
+    client.get('statuses/user_timeline', (err, tweets, data) =>{
+        if (err) {throw err};
+        console.log(data);
+    })
+};
+
+//use node liri.js spotify-this-song '<song name here>' to:
+//show artist/name/preview link/ album
+spotify =>{
+
+};
+
+//use node liri.js movie-this '<movie name here>' to:
+//display movie title/year/imdb rating/ rotten tomatoes rating/ country/ lang/ plot/ actors
+movie =>{
+
+};
+
+//use node liri.js do-what-it-says to:
+//take text inside random.txt, using it to call from random.txt
+doWhat =>{
+
+};
