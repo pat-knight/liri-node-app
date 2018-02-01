2a50d17f979199aa33cce7b6441e10f54026a554
Patrick Knight <ptknight00@gmail.com>
2018-01-31 20:48:28 -0600

    log.txt/format

--- liri.js
+++ liri.js
@@ -7,57 +7,70 @@ const twitter = require("twitter");
 const Spotify = require('node-spotify-api');
 const userCommand = process.argv[2];
 
-switch(userCommand){
+switch (userCommand) {
     case 'my-tweets':
         tweets();
-    break;
-    
+        break;
+
     case 'spotify-this-song':
         spotify();
-    break;
-    
+        break;
+
     case 'movie-this':
         movie();
-    break;
-    
+        break;
+
     case 'do-what-it-says':
         doWhat();
-    break;
+        break;
 }
 
 //use node liri.js my-tweets to:
 //show last 20 tweets with timestamp in bash window
-function tweets(){
+function tweets() {
     var client = new twitter(keys.twitter);
     var tweets = {
         user_id: 958856873649811459,
         count: 5
     };
 
-    client.get('statuses/user_timeline', (err, tweets, response) =>{
-        if (err) {throw err};
+    client.get('statuses/user_timeline', (err, tweets, response) => {
+        if (err) throw err;
         // console.log(response);
-        var tweetLog = 
-        tweets.forEach(function(element){
-            console.log(`\n Tweet: ${element.text} \n time: ${element.created_at}`);
-        })
+        var tweetLog =
+            tweets.forEach(function (element) {
+                var content = element.text;
+                var stamp = element.created_at
+                // console.log(`\n Tweet: ${element.text} \n Time: ${element.created_at}`);
+                console.log(`\n Tweet: ${content} \n Time: ${stamp}`)
+                
+                
+                fs.appendFile('log.txt', `\n Tweet: ${content} \n Time: ${stamp}`, function (err) {
+                    if (err) throw err;
+
+                    else {
+                        console.log("Content Added!");
+                    }
+
+                });
+            });
     })
 };
 
 //use node liri.js spotify-this-song '<song name here>' to:
 //show artist/name/preview link/ album
-spotify =>{
+spotify => {
 
 };
 
 //use node liri.js movie-this '<movie name here>' to:
 //display movie title/year/imdb rating/ rotten tomatoes rating/ country/ lang/ plot/ actors
-movie =>{
+movie => {
 
 };
 
 //use node liri.js do-what-it-says to:
 //take text inside random.txt, using it to call from random.txt
-doWhat =>{
+doWhat => {
 
 };
